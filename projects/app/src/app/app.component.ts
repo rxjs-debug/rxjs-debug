9330485a2b424ec8eac2eeb8f062d7f9c29bbe6c
dabalyan <dabalyan@hotmail.com>
2020-11-14 23:51:19 +0100

    website with playground

--- projects/app/src/app/app.component.ts
+++ projects/app/src/app/app.component.ts
@@ -1,8 +1,26 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild, ViewEncapsulation} from '@angular/core';
+import sdk from '@stackblitz/sdk';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent {}
+export class AppComponent implements AfterViewInit {
+  @ViewChild('stackBlitzHost') stackBlitzHost: ElementRef<HTMLDivElement>;
+
+  ngAfterViewInit(): void {
+    sdk.embedProjectId(this.stackBlitzHost.nativeElement, 'rxjs-debug-playground', {
+      openFile: new URLSearchParams(location.search).get('file') || 'index.ts',
+      view: 'both',
+      theme: 'dark',
+      width: '100%',
+      height: '100%',
+      hideDevTools: true,
+      hideExplorer: true,
+      hideNavigation: true,
+      forceEmbedLayout: true,
+    });
+  }
+}
